f1aba9c6aae50abaf4f34d35b92cc1e4ac90fb4c
Pingvu <nhlpool27@gmail.com>
2024-02-14 12:57:03 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace forre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 3;
+            double y = 3;
+
+            Console.WriteLine(Potens(x, y));
+
+        }
+
+        static double Potens(double x, double y)
+        {
+            double svar = 1;
+            for (double i = 0; i < y; i++)
+            {
+                svar = x * svar;
+            }
+
+            return svar;
+        }
+    }
+}
